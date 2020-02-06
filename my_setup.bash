35d08e106f7717a3d0672c37d895c1cc17d2717f
JBuzzatto <jsan819@aucklanduni.ac.nz>
2020-02-06 15:54:56 +1300

    deleted gazebo sitl .git files

--- my_setup.bash
+++ my_setup.bash
@@ -1,5 +1,5 @@
 #cd ~/Firmware
-DONT_RUN=1 make px4_sitl_default gazebo
+DONT_RUN=1 make px4_sitl_default gazebo #build but dont run
 source ~/catkin_ws/devel/setup.bash    
 source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
